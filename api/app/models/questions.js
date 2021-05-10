1b8c6946bf185ea0445fd0a255d691bb7583dba7
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-10 19:42:53 +0700

    Fix id's so there is a relationship between tables

--- api/app/models/questions.js
+++ api/app/models/questions.js
@@ -2,27 +2,27 @@ module.exports = [
   {
     id: '6030437e-b3ac-4dc0-8340-3666c170fdf0',
     title: 'What is the difference between req.params and req.query?',
-    quizId: 'd7e1dc5e-1feb-4fb4-bf7b-b6a9817be4cd',
+    quizId: 'a760c0da-8de7-45a4-9601-63a3b607804f',
   },
   {
     id: '33b3810d-0307-4ef4-8e30-b2ab4159efe6',
     title: 'What is a UUID?',
-    quizId: '5385ae0b-6698-4b44-8009-7aa9ea9dc7e2',
+    quizId: '44f22531-de40-4d21-ae16-40d758b22c40',
   },
   {
     id: 'f30f433a-5f9d-4e3f-8a7c-5fc399def13c',
     title:
       'What is the package manager that we use to install packages for our application?',
-    quizId: '7fd14794-c176-4c1b-98ea-8ce32d0239c0',
+    quizId: 'c35866ac-7857-48fb-827d-d016af7ce677',
   },
   {
     id: '60494e8b-fd81-4694-81c3-b15b94fe5026',
     title: 'What is a GET request?',
-    quizId: 'ddf66e1c-8bd1-4c15-8a08-665a197a6106',
+    quizId: '38b181d3-d06c-4ada-ae00-0d935e3d6295',
   },
   {
     id: '1a4b1473-1180-4d72-9c5c-b10254c3fa73',
     title: 'What are some examples of a REST API?',
-    quizId: '11f744d8-b235-4412-ac95-bba56c108519',
+    quizId: '2be7b6c3-c1e6-4739-a21e-f3a7594d9816',
   },
 ];
