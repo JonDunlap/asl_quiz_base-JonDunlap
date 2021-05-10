f038ad5b9aba0c125a940b7296c80741c3bea49b
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-10 19:39:01 +0700

    Populate the choices model with example data

---
+++ api/app/models/choices.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    id: '33e625c7-ee93-4091-9520-51104b124e81',
+    value: 'A random number between 0 and 100.',
+    type: 'incorrect',
+    questionId: '5385ae0b-6698-4b44-8009-7aa9ea9dc7e2',
+  },
+  {
+    id: '49748aed-8d6c-451c-8200-fe4759f33a3a',
+    value: 'A series of random letters.',
+    type: 'incorrect',
+    questionId: '5385ae0b-6698-4b44-8009-7aa9ea9dc7e2',
+  },
+  {
+    id: '27a7707c-fd89-4b3a-8210-ee2a9b09978a',
+    value: 'A time code used as an ID.',
+    type: 'incorrect',
+    questionId: '5385ae0b-6698-4b44-8009-7aa9ea9dc7e2',
+  },
+  {
+    id: '440f6a6d-1867-43ab-b0b6-0d54408cda33',
+    value: 'A 128-bit label used for information in computer systems.',
+    type: 'correct',
+    questionId: '5385ae0b-6698-4b44-8009-7aa9ea9dc7e2',
+  },
+  {
+    id: 'b6ae9365-f3dd-4bad-a250-a3d4f9485395',
+    value: 'Yarn',
+    type: 'incorrect',
+    questionId: '7fd14794-c176-4c1b-98ea-8ce32d0239c0',
+  },
+  {
+    id: 'a36e0cd0-c02e-4dd5-beed-ff3c61e0954c',
+    value: 'React',
+    type: 'incorrect',
+    questionId: '7fd14794-c176-4c1b-98ea-8ce32d0239c0',
+  },
+  {
+    id: '231246f0-15b2-46e1-a2f4-c1edee0efcbb',
+    value: 'Bower',
+    type: 'incorrect',
+    questionId: '7fd14794-c176-4c1b-98ea-8ce32d0239c0',
+  },
+  {
+    id: 'adefe081-be7c-4fbe-8ad3-fdf463e663d1',
+    value: 'npm: Node package manager',
+    type: 'correct',
+    questionId: '7fd14794-c176-4c1b-98ea-8ce32d0239c0',
+  },
+  {
+    id: 'ac4c1b51-2e6e-42dc-b43b-d0cd6fb2d1a9',
+    value: 'A way to send data to a server to create/update a resource.',
+    type: 'incorrect',
+    questionId: 'ddf66e1c-8bd1-4c15-8a08-665a197a6106',
+  },
+  {
+    id: '8af2f85e-d0d4-46fd-8492-528760f068a0',
+    value: 'A way to delete a specified resource.',
+    type: 'incorrect',
+    questionId: 'ddf66e1c-8bd1-4c15-8a08-665a197a6106',
+  },
+  {
+    id: 'eec52886-dc2e-4be8-beb4-26c6253f0b68',
+    value: 'Describes the communication options for the target resource.',
+    type: 'incorrect',
+    questionId: 'ddf66e1c-8bd1-4c15-8a08-665a197a6106',
+  },
+  {
+    id: '19b7d34a-94ed-4715-b09c-99b5b827d172',
+    value: 'Used to request data from a specified resource.',
+    type: 'correct',
+    questionId: 'ddf66e1c-8bd1-4c15-8a08-665a197a6106',
+  },
+];
