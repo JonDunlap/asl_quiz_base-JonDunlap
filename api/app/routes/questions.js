c8e018fc3baff7ce895b25f5e3adea0eb18f0ea1
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-10 20:03:49 +0700

    Create routes for the questions

---
+++ api/app/routes/questions.js
@@ -0,0 +1,19 @@
+// import the express router
+const router = require('express').Router();
+
+// import the controller
+const questionsController = require('../controllers/questions');
+
+// GET /questions?quizId
+router.get('/', questionsController.getQuestionByQuizId);
+// GET /questions/:id
+router.get('/:id', questionsController.getQuestion);
+// POST /questions
+router.post('/', questionsController.createQuestion);
+// PUT /questions/:id
+router.put('/:id', questionsController.updateQuestion);
+// DELETE /questions/:id
+router.delete('/:id', questionsController.deleteQuestion);
+
+// export the router from this file
+module.exports = router;
