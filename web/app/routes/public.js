6e97238bde1ca21602251afa0f68921c3e4e3338
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-15 17:14:01 +0700

    Add public route for getting quiz list by id

--- web/app/routes/public.js
+++ web/app/routes/public.js
@@ -6,6 +6,8 @@ const quizzesController = require('../controllers/quizzes');
 
 // GET / - loads the home page
 router.get('/', quizzesController.renderLanding);
+// GET /quiz/:id - loads the questions and choices for the given quiz
+router.get('/quiz/:id', quizzesController.renderDashboard);
 
 // export the router
 module.exports = router;
