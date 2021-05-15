a6272499b20e817ea71195ceaab813cda67d51a2
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-15 20:35:15 +0700

    Add control methods for implemented routes

--- web/app/controllers/quizzes.js
+++ web/app/controllers/quizzes.js
@@ -13,6 +13,76 @@ exports.renderQuestionsList = async (req, res) => {
   // get the questions for this quiz
   const questions = await req.API.get(`/questions?quizId=${id}`);
 
+  res.render('quizzes/list', { quiz, questions });
+};
+
+exports.renderQuizForm = async (req, res) => {
+  res.render('quizzes/form', { name: '', type: 'private' });
+};
+
+// four params are required to mark this as an error handling middleware
+// eslint-disable-next-line no-unused-vars
+exports.renderQuizFormWithErrors = (errors, req, res, next) => {
+  // get the data the user submitted
+  const { name, type } = req.body;
+
+  // send the title, type, and errors as variables to the view
+  res.render('quizzes/form', { name, type, errors });
+};
+
+exports.saveQuiz = async (req, res) => {
+  // get the data the user submitted
+  const { name, type } = req.body;
+  // pull the id from the request params
+  const { id } = req.params;
+
+  // variable to hold the data from our api request
+  let data = {};
+
+  // if there is an id, we are editing, if there isn't we are adding
+  if (id) {
+    // make a put request with the updated information
+    data = await req.API.put(`/quizzes/${id}`, { name, type });
+  } else {
+    // send the new quiz to the API
+    data = await req.API.post('/quizzes', { name, type });
+  }
+
+  // redirect to the edit quiz detail page
+  res.redirect(`/admin/quizzes/${data.id}`);
+};
+
+exports.renderEditForm = async (req, res) => {
+  // get the id from the request params
+  const { id } = req.params;
+
+  // get the details of the quiz
+  const quiz = await req.API.get(`/quizzes/${id}`);
+
+  // render the edit form
+  res.render('quizzes/form', quiz);
+};
+
+exports.renderDashboard = async (req, res) => {
+  // TODO - add userId of logged in user
+  const quizzes = await req.API.get('/quizzes');
+
+  //! DEBUG - used to show buttons for logged in user
   const loggedIn = true;
-  res.render('quizzes/list', { quiz, questions, loggedIn });
+
+  res.render('quizzes/admin-landing', { quizzes, loggedIn });
+};
+
+exports.renderAdminDetail = async (req, res) => {
+  const { id } = req.params;
+
+  // get the detail of this quiz
+  const quiz = await req.API.get(`/quizzes/${id}`);
+  // get the questions for this quiz
+  const questions = await req.API.get(`/questions?quizId=${id}`);
+
+  //! DEBUG - used to show buttons for logged in user
+  const loggedIn = true;
+
+  res.render('quizzes/detail', { quiz, questions, loggedIn });
 };
