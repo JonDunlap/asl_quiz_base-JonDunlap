942cb6018765366e14e7f0958deef0dddbe4c5e3
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-16 23:17:08 +0700

    Create POST route for the public quiz

--- web/app/routes/public.js
+++ web/app/routes/public.js
@@ -3,11 +3,18 @@ const router = require('express').Router();
 
 // load the controller
 const quizzesController = require('../controllers/quizzes');
+const validationCtrl = require('../controllers/validation');
 
 // GET / - loads the home page
 router.get('/', quizzesController.renderLanding);
 // GET /quiz/:id - loads the questions and choices for the given quiz
 router.get('/quiz/:id', quizzesController.renderQuestionsList);
+// POST /quiz/:id - shows the results of the quiz
+router.post('/quiz/:id', [
+  validationCtrl.validate('submitChoice'),
+  quizzesController.renderQuestionsListWithErrors,
+  quizzesController.renderQuestionsListWithSuccess,
+]);
 
 // export the router
 module.exports = router;
