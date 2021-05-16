e71cef501ae49bbbdff9f87a04d650d499d459bc
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-16 17:30:26 +0700

    Edit keys to link questions with quizzes

--- api/app/models/questions.js
+++ api/app/models/questions.js
@@ -1,28 +1,29 @@
 module.exports = [
   {
-    id: '6030437e-b3ac-4dc0-8340-3666c170fdf0',
-    title: 'What is the difference between req.params and req.query?',
-    quizId: 'a760c0da-8de7-45a4-9601-63a3b607804f',
-  },
-  {
     id: '33b3810d-0307-4ef4-8e30-b2ab4159efe6',
     title: 'What is a UUID?',
-    quizId: '44f22531-de40-4d21-ae16-40d758b22c40',
+    quizId: 'a760c0da-8de7-45a4-9601-63a3b607804f',
   },
   {
     id: 'f30f433a-5f9d-4e3f-8a7c-5fc399def13c',
     title:
       'What is the package manager that we use to install packages for our application?',
-    quizId: 'c35866ac-7857-48fb-827d-d016af7ce677',
+    quizId: 'a760c0da-8de7-45a4-9601-63a3b607804f',
   },
   {
     id: '60494e8b-fd81-4694-81c3-b15b94fe5026',
     title: 'What is a GET request?',
-    quizId: '38b181d3-d06c-4ada-ae00-0d935e3d6295',
+    quizId: '44f22531-de40-4d21-ae16-40d758b22c40',
   },
+  {
+    id: '6030437e-b3ac-4dc0-8340-3666c170fdf0',
+    title: 'What is the difference between req.params and req.query?',
+    quizId: '44f22531-de40-4d21-ae16-40d758b22c40',
+  },
+
   {
     id: '1a4b1473-1180-4d72-9c5c-b10254c3fa73',
     title: 'What are some examples of a REST API?',
-    quizId: '2be7b6c3-c1e6-4739-a21e-f3a7594d9816',
+    quizId: '44f22531-de40-4d21-ae16-40d758b22c40',
   },
 ];
