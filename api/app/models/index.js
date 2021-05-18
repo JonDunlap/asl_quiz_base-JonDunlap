68fb6ad990fe1730b27a77a7222dbec8c20b1b11
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-18 14:16:39 +0700

    Initialize Sequelize

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -1,67 +1,37 @@
-const { v1: uuid } = require('uuid');
-const choices = require('./choices');
-const questions = require('./questions');
-const quizzes = require('./quizzes');
+'use strict';
 
-class Model {
-  constructor(data) {
-    this.values = data;
-  }
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../db/config.json')[env];
+const db = {};
 
-  // METHODS
-
-  // Get all of the items
-  getAll() {
-    return this.values;
-  }
-
-  // Get a single item by id
-  getOneById(id) {
-    return this.values.find((item) => item.id === id);
-  }
-
-  // create a new item
-  createNewItem(item) {
-    // create a uuid
-    const id = uuid();
-    // add the item to the array along with the uuid
-    this.values.push({ id, ...item });
-
-    // return the new id
-    return id;
-  }
-
-  // update an item using the id and the new values
-  updateItem(valuesToChange, id) {
-    // get the index of the item that we want to update
-    const index = this.values.findIndex((item) => item.id === id);
-    // take the current values and update them with the new values
-    const updatedValues = { ...this.values[index], ...valuesToChange };
-
-    // piece together the array with all the previous items, the new one, and the items after
-    this.values = [
-      ...this.values.slice(0, index),
-      updatedValues,
-      ...this.values.slice(index + 1),
-    ];
-
-    // return the updated values
-    return updatedValues;
-  }
-
-  // delete an item by id
-  deleteItem(id) {
-    // filter the values to remove the deleted item
-    this.values = this.values.filter((item) => {
-      if (item.id === id) return false;
-      return true;
-    });
-  }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// Export the Model for each route
-module.exports = {
-  Choices: new Model(choices),
-  Questions: new Model(questions),
-  Quizzes: new Model(quizzes),
-};
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
