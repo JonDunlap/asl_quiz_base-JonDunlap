97e0a85d286c841701f1619f544510711b434e77
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-22 12:42:53 +0700

    Add validation to data types

--- api/app/models/users.js
+++ api/app/models/users.js
@@ -2,10 +2,43 @@ module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define(
     'Users',
     {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      access_token: DataTypes.STRING,
-      type: DataTypes.ENUM('github', 'regular'),
+      id: {
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        validate: {
+          isUUID: {
+            args: 4,
+            msg: 'ID is not valid, please go back and try again.',
+          },
+        },
+      },
+
+      username: {
+        type: DataTypes.STRING,
+        unique: { args: true, msg: 'Username is already in use.' },
+        allowNull: { args: false, msg: 'Username is required.' },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      access_token: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      type: {
+        type: DataTypes.ENUM('github', 'regular'),
+        validate: {
+          isIn: {
+            args: [['github', 'regular']],
+            msg: 'User type must be GitHub or regular',
+          },
+        },
+      },
     },
     {}
   );
