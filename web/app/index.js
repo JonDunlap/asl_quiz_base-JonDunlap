cb017ba158c29e035d3fdf05516d414d8491018d
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-22 13:12:23 +0700

    Add middleware for sessions

--- web/app/index.js
+++ web/app/index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 // add another logger
 const error = require('debug')('web:error');
+// middleware for sessions
+const expressSession = require('express-session');
+// store for saving sessions
+const FileStore = require('session-file-store')(expressSession);
 // load in the axios middleware
 const API = require('./utils/API');
 
@@ -13,6 +17,20 @@ const adminQuizzesRoutes = require('./routes/adminQuizzes');
 
 // create an express app
 const app = express();
+
+// session middleware
+app.use(
+  expressSession({
+    // another secret used for encoding sessions data
+    secret: process.env.SECRET,
+    // should the session save again if nothing has changed?
+    resave: false,
+    // should sessions be created if they have no data?
+    saveUninitialized: false,
+    // where to store the session data
+    store: new FileStore(),
+  })
+);
 
 // setup a folder to hold all the static files
 app.use(express.static('public'));
