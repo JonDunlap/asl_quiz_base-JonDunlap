d88b355aaa4dd532e4cec6b2684d4b57c08527fe
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-23 15:28:46 +0700

    Update comments

--- api/app/routes/auth.js
+++ api/app/routes/auth.js
@@ -6,7 +6,7 @@ const authCtrl = require('../controllers/auth');
 
 // POST /auth/exchange - receives a code and will exchange it for an access token
 router.post('/exchange', authCtrl.exchangeCode);
-// GET /auth/token
+// GET /auth/token - exchange the access token for user information
 router.post('/token', authCtrl.exchangeAccessToken);
 
 // export the route from this file
