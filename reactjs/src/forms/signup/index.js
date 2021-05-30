bc74c2e6766f15cc978deb9943b7c31badb0fa82
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-05-30 20:35:47 +0700

    Add comments for signup component

--- reactjs/src/forms/signup/index.js
+++ reactjs/src/forms/signup/index.js
@@ -25,6 +25,7 @@ class Login extends React.Component {
     });
   };
 
+  // Sends username and password to the HOC for signup
   signUp = async (event) => {
     // don't actually submit the form through the browser
     event.preventDefault();
